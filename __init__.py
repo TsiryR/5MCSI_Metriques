6d58db75699accb3da3cdb887e1dc1947e3c4546
TsiryR <114159363+TsiryR@users.noreply.github.com>
2024-06-03 10:09:41 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route("/histogramme/")
-def hello_world():
+def monhistogramme():
     return render_template("test.html")
 
 if __name__ == "__main__":
